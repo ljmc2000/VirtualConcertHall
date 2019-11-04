68a3abb84075c6d254c83e5d293fa9e2278fdc5a
Liam McCormick <ljmc2000@gmail.com>
2019-11-04 16:56:02 +0000

    removed c++ server due to potential change of stack

--- RoomServer/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -I/usr/include/webrtc -std=c++11 -DWEBRTC_POSIX -o roomServer main.cpp /usr/lib/libwebrtc_full.a
--- RoomServer/prebuild.sh
+++
@@ -1,8 +0,0 @@
-mkdir prebuild
-cd prebuild
-wget https://github.com/sourcey/webrtc-precompiled-builds/raw/master/webrtc-22215-ab42706-linux-x64.tar.gz
-tar xvf webrtc-*-linux-x64.tar.gz
-cp -r lib/x64/Release/* /usr/lib
-mkdir /usr/include/webrtc
-cp -r include/* /usr/include/webrtc
-cd ..
