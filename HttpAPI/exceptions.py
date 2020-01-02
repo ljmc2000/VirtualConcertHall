87aa483a7c94a3134b32d124b4bcf37c3cb63568
Liam McCormick <ljmc2000@gmail.com>
2020-01-02 17:40:59 +0000

    added error message for bad request

--- HttpAPI/exceptions.py
+++ HttpAPI/exceptions.py
@@ -1,5 +1,5 @@
 from mongoengine.queryset import NotUniqueError,DoesNotExist
-
+from werkzeug.exceptions import BadRequest
 
 import re	#my patience wears thin for people who write exceptions which include a message and nothing else so you must dig through the message with regular exceptions
 notUniqueErrorRegex=re.compile('Tried to save duplicate unique keys (\(([\w\d]*) duplicate key error collection: \w*\.(\w*) .*)')
@@ -15,16 +15,18 @@ def handleException(app, e: Exception, endpoint: str):
 	try:
 		app.logger.error(type(e).__name__+' '+str(e))
 		raise e
-	except ExpiredLoginToken:
+	except ExpiredLoginToken as e:
 		return {"status":"failure","reason":"Your login token has expired"}
 	except NotUniqueError as e:
 		usefulInformation=notUniqueErrorRegex.search(str(e))
 		return {"status":"failure","reason":"A duplicate %s exists" % usefulInformation.group(3)}
 	except DoesNotExist as e:
 		usefulInformation=doesNotExistRegex.search(str(e))
-		return {"status":"failure","reason":"no such %s exists" % usefulInformation.group(1)}
+		return {"status":"failure","reason":"No such %s exists" % usefulInformation.group(1)}
+	except BadRequest as e:
+		return {"status":"failure","reason":"A malformed or otherwise invalid request was made to the server"}
 	except Exception as e:
 		from databaseClasses import UnexpectedError
 		error=UnexpectedError(type=type(e).__name__, message=str(e), endpoint=endpoint)
 		error.save()
-		return {"status":"failure","reason":"an unknown error has occured"}
+		return {"status":"failure","reason":"An unknown error has occured"}
