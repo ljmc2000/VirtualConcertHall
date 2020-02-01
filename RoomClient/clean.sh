75e141d4d66a70df3730484b3715050d1e734d06
Liam McCormick <ljmc2000@gmail.com>
2020-02-01 13:41:23 +0000

    added a script to generate a zip file containing all reqired libs for running on windows

--- RoomClient/clean.sh
+++ RoomClient/clean.sh
@@ -1,2 +1,2 @@
 make clean
-rm -r RoomClient Makefile .qmake.stash RoomClient.pro.user release debug client_plugin_import.cpp Makefile\.* clean
+rm -r RoomClient Client.exe Makefile .qmake.stash RoomClient.pro.user release debug client_plugin_import.cpp Makefile\.* clean
